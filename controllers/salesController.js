1d74c250dca2e1c555e85065d47b2c74146b791c
Ingrid_Ramos <ingridrcs@gmail.com>
2022-06-02 13:02:14 -0300

    controller e service de sales

---
+++ controllers/salesController.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const router = express.Router();
+const salesService = require('../services/salesService');
+
+router.get('/sales', async (req, res) => {
+  try {
+    const [rows] = await salesService.getAll();
+    return res.status(200).json(rows); 
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json({ message: 'Erro interno' });
+  }
+});
+
+router.get('/products/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await salesService.getById(id);
+    return res.status(200).json(response); 
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json({ message: 'Product not found' });
+  }
+});
