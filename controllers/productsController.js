ee97f0f6afae2432a9fe176e369c139128686bc8
Ingrid_Ramos <ingridrcs@gmail.com>
2022-06-02 22:31:50 -0300

    alteraçoes no controller

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,27 +1,24 @@
-const express = require('express');
-
-const router = express.Router();
 const productsService = require('../services/productsService');
 
-router.get('/products', async (req, res) => {
+const getAll = async (_req, res) => {
   try {
-    const [rows] = await productsService.getAll();
-    return res.status(200).json(rows); 
-  } catch (error) {
-    console.log(error);
-    return res.status(404).json({ message: 'Product not found' });
-  }
-});
-
-router.get('/products/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const response = await productsService.getById(id);
+    const response = await productsService.getAll();
     return res.status(200).json(response); 
   } catch (error) {
     console.log(error);
     return res.status(404).json({ message: 'Product not found' });
   }
-});
+};
 
-module.exports = router;
+const getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await productsService.getAll(id);
+    return res.status(200).json(response); 
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json({ message: 'Product not found' });
+  }
+};
+
+module.exports = { getAll, getById };
--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,25 +1,24 @@
-const express = require('express');
-
-const router = express.Router();
 const salesService = require('../services/salesService');
 
-router.get('/sales', async (_req, res) => {
+const getAll = async (_req, res) => {
   try {
-    const [rows] = await salesService.getAll();
-    return res.status(200).json(rows); 
+    const response = await salesService.getAll();
+    return res.status(200).json(response); 
   } catch (error) {
     console.log(error);
-    return res.status(404).json({ message: 'Erro interno' });
+    return res.status(200).json({ message: 'Sale not found' });
   }
-});
+};
 
-router.get('/products/:id', async (req, res) => {
+const getById = async (req, res) => {
   try {
     const { id } = req.params;
     const response = await salesService.getById(id);
     return res.status(200).json(response); 
   } catch (error) {
     console.log(error);
-    return res.status(404).json({ message: 'Product not found' });
+    return res.status(200).json({ message: 'Sale not found' });
   }
-});
+};
+
+module.exports = { getAll, getById };
