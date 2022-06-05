3ff29dc8678a1e89f19713c20e7e0f15b9df09b0
Ingrid_Ramos <ingridrcs@gmail.com>
2022-06-04 21:51:27 -0300

    alteracoes para resolver erro

--- middlewares/productMiddleware.js
+++ middlewares/productMiddleware.js
@@ -1,18 +1,18 @@
-// const productValidation = (req, res, next) => {
-//   const { name, quantity } = req.body;
-//   if (!name) {
-//     return res.status(400).json({ message: '"name" is required' });
-//   }
-//   if (name.length < 5) {
-//     return res.status(422).json({ message: '"name" length must be at least 5 characters long"' });
-//   }
-//   if (!quantity) {
-//     return res.status(400).json({ message: '"quantity" is required' });
-//   }
-//   if (quantity <= 0) {
-//     return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
-//   }
-//   next();
-// };
+const productValidation = (req, res, next) => {
+  const { name, quantity } = req.body;
+  if (!name) {
+    return res.status(400).json({ message: '"name" is required' });
+  }
+  if (name.length < 5) {
+    return res.status(422).json({ message: '"name" length must be at least 5 characters long' });
+  }
+  if (!quantity) {
+    return res.status(400).json({ message: '"quantity" is required' });
+  }
+  if (quantity <= 0) {
+    return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
+  }
+  next();
+};
 
-// module.export = productValidation;
+module.exports = productValidation;
