f9ff484c5b4aa615cc47b96e06fbca3742c88961
Ingrid_Ramos <ingridrcs@gmail.com>
2022-06-04 21:53:47 -0300

    colcoacao das rotas post

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -4,8 +4,11 @@ const router = express.Router();
 
 const productsController = require('../controllers/productsController');
 
+const validationProduct = require('../middlewares/productMiddleware');
+
 router.get('/products', productsController.getAllItens);
 router.get('/products/:id', productsController.getByIdItens);
-// router.post('/products', productsController);
+router.post('/products', validationProduct, productsController.addItens);
+// router.put('/products/:id', productsController.updateItens);
 
 module.exports = router;
--- routes/salesRoutes.js
+++ routes/salesRoutes.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 const salesController = require('../controllers/salesController');
 
+const salesValidation = require('../middlewares/salesMiddleware');
+
 router.get('/sales', salesController.getAllItens);
 router.get('/sales/:id', salesController.getByIdItens);
-// router.post('/sales', salesController);
+router.post('/sales', salesValidation, salesController.addItens);
 
 module.exports = router;
