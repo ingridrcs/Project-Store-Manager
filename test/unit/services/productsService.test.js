6786a3e22351653cbff03ccafe6cf4fcd321f578
Ingrid_Ramos <ingridrcs@gmail.com>
2022-06-09 03:22:30 -0300

    testes da camada services

--- test/unit/services/productsService.test.js
+++ test/unit/services/productsService.test.js
@@ -5,56 +5,80 @@ const productsService = require("../../../services/productsService");
 
 describe("Testando a camada de Service", () => {
   describe("testando a rota /products/:id onde o produto é retornado pelo id", async () => {
-    const id = null;
+    const id = 1;
     const nameProduct = "produto";
     const quantity = 3;
     before(async () => {
-      const findId = [{
+      const findId = [[{
         "id": 1,
         "name": "produto",
         "quantity": 10
-      }]
+      }]]
 
       sinon.stub(productsModel, "getById").resolves(findId)
     });
 
     after(async () => { productsModel.getById.restore() });
 
-    it("retorna um objeto", async () => {
+    it("retorna um array", async () => {
       const [response] = await productsService.getProductService(id);
 
-      expect(response).to.be.an("object");
+      expect(response).to.be.an("array");
     });
     it("Retorna as chaves 'id, 'name' e 'quantity'", async () => {
       const [response] = await productsService.getProductService(nameProduct, quantity);
-      expect(response).to.have.property("id");
-      expect(response).to.have.property("name");
-      expect(response).to.have.property("quantity");
+      expect(response[0]).to.have.property("id");
+      expect(response[0]).to.have.property("name");
+      expect(response[0]).to.have.property("quantity");
     })
   });
-  describe("Testando a rota /products para adição de um produto", () => {
-    describe("retorno de um ", () => {
-      before(async () => {
-        const findId = [{
-          "id": 1,
-          "name": "produto",
-          "quantity": 10
-        }]
-        sinon.stub(productsModel, "getByName").resolves(findId)
-      });
-      after(async () => { productsModel.getByName.restore() });
-    })
-    it("retorna um objeto", async () => {
-      const nameProduct = "produtoA";
-      const quantity = 3;
-      const response = await productsService.addProductService(nameProduct, quantity);
-
-      expect(response).to.be.an("object");
+  describe('quando não há produtos cadastrados', async () => {
+    before(() => {
+      const empty = [[]];
+      sinon.stub(productsModel, 'getAll').resolves(empty);
+    });
+    after(() => {
+      productsModel.getAll.restore();
+    });
+    it('o array retorna como vazio', async () => {
+      const response = await productsService.getProductService();
+      expect(response[0]).to.be.empty;
+    });
+    it('retorna um array', async () => {
+      const response = await productsService.getProductService();
+      expect(response).to.be.an('array');
     });
 
-    // it('se "name" retorna uma string', async () => {
-    //   const response = await productsModel.getByName(insertName);
-    //   expect(response.name).to.be.a("string");
-    // });
-  })
+  });
+  describe("Testando a atualização de um produto", async () => {
+    const allProducts = [[{
+      "id": 1,
+      "name": "Martelo",
+      "quantity": 10
+    },
+    {
+      "id": 2,
+      "name": "Traje",
+      "quantity": 20
+    },
+    {
+      "id": 3,
+      "name": "Escudo",
+      "quantity": 30
+    }]]
+    before(async () => {
+      sinon.stub(productsModel, "getById").resolves(allProducts);
+      sinon.stub(productsModel, "update").resolves(allProducts);
+    });
+    after(async () => { productsModel.getById.restore(), productsModel.update.restore() });
+
+    it("atualiza um produto e retorna um objeto", async () => {
+      const id = 1;
+      const nameProduct = "produto";
+      const quantity = 10;
+      const response = await productsService.updateProductService(id, nameProduct, quantity);
+      expect(response[0][0]).to.be.an("object");
+    });
+  });
+
 })
---
+++ test/unit/services/salesService.test.js
@@ -0,0 +1,89 @@
+const sinon = require("sinon");
+const { expect } = require("chai");
+const salesModel = require("../../../models/salesModel");
+const salesService = require("../../../services/salesService");
+
+describe("Testando a camada de Service", () => {
+  describe("testando a rota /sales/:id onde a venda é retornada pelo id", async () => {
+    const id = 1;
+    before(async () => {
+      const findId = [[{
+        "saleId": 1,
+        "date": "2022-06-08T19:27:30.000Z",
+        "productId": 1,
+        "quantity": 5
+      }]]
+
+      sinon.stub(salesModel, "getById").resolves(findId)
+    });
+
+    after(async () => { salesModel.getById.restore() });
+
+    it("retorna um array", async () => {
+      const response = await salesService.getSaleService(id);
+
+      expect(response).to.be.an("array");
+    });
+    it("Retorna as chaves 'saleId, 'date' 'productId' e 'quantity'", async () => {
+      const [response] = await salesService.getSaleService(id);
+      expect(response[0]).to.have.property("saleId");
+      expect(response[0]).to.have.property("date");
+      expect(response[0]).to.have.property("quantity");
+      expect(response[0]).to.have.property("productId");
+    })
+  });
+  describe('quando não há vendas cadastradas', async () => {
+    before(() => {
+      const empty = [[]];
+      sinon.stub(salesModel, 'getAll').resolves(empty);
+    });
+    after(() => {
+      salesModel.getAll.restore();
+    });
+    it('o array retorna como vazio', async () => {
+      const response = await salesService.getSaleService();
+      expect(response[0]).to.be.empty;
+    });
+    it('retorna um array', async () => {
+      const response = await salesService.getSaleService();
+      expect(response).to.be.an('array');
+    });
+
+  });
+  describe("Testando a atualização de uma venda", async () => {
+    const salelist = [[
+      {
+        "saleId": 1,
+        "date": "2022-06-08T19:27:30.000Z",
+        "productId": 1,
+        "quantity": 5
+      },
+      {
+        "saleId": 1,
+        "date": "2022-06-08T19:27:30.000Z",
+        "productId": 2,
+        "quantity": 10
+      },
+      {
+        "saleId": 2,
+        "date": "2022-06-08T19:27:30.000Z",
+        "productId": 3,
+        "quantity": 15
+      }
+    ]];
+    before(async () => {
+      sinon.stub(salesModel, "getById").resolves(salelist);
+      sinon.stub(salesModel, "update").resolves(salelist);
+    });
+    after(async () => { salesModel.getById.restore(), salesModel.update.restore() });
+
+    it("atualiza uma venda e retorna um objeto", async () => {
+      const id = 1;
+      const productId = 2;
+      const quantity = 10;
+      const response = await salesService.updateSalesService(id, productId, quantity);
+      expect(response[0][0]).to.be.an("object");
+    });
+  });
+
+})
